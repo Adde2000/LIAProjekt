de08a850cefee40b1673fbe1a42e5a62cbeccd78
Alexander <97alegre@gafe.molndal.se>
2026-05-20 12:04:36 +0200

    Cleanup

--- Backend/src/main/java/se/liaprojekt/dto/CourseRequest.java
+++ Backend/src/main/java/se/liaprojekt/dto/CourseRequest.java
@@ -1,9 +1,5 @@
 package se.liaprojekt.dto;
 
-import java.time.LocalDateTime;
-
-import static org.springframework.data.jpa.domain.AbstractAuditable_.createdBy;
-
 public record CourseRequest(
 //        Long id,
         String title,
